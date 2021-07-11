dae8d7abfe0c1888036bd26940d13f71bfa92d95
asmwarrior <asmwarrior@gmail.com>
2021-07-11 15:31:13 +0800

    use the correct glm header files

--- GLPane.h
+++ GLPane.h
@@ -12,7 +12,13 @@ class Object;
 #include <wx/wx.h>
 #include <wx/glcanvas.h>
 
-#include <glm.hpp>
+// #include <glm.hpp>
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/ext/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale
+#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
+#include <glm/ext/scalar_constants.hpp> // glm::pi
 
 #include <chrono>
 
